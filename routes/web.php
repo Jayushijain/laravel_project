fe360ea1822719d232e1f3385c103d3ac51d6a68
Mohmed Asvat <mua@narola.email>
2020-03-09 10:57:31 +0530

    frontend design done

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::resource('/admin','AdminController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
